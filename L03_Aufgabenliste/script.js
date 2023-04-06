e21d036dde40055c89397e52e7fcc86424ff5dfa
Dominik-art-cpu <115712617+Dominik-hfu@users.noreply.github.com>
2023-04-06 17:33:17 +0200

    haushaltsplan

--- L03_Aufgabenliste/script.js
+++ L03_Aufgabenliste/script.js
@@ -9,11 +9,11 @@ var organizer;
     // task.addEventListener('keyup', function(){
     //     console.log("you created a new task")
     // })
-    let inputField = document.querySelector("input"); // Zugriff auf Input-Feld Element
+    let inputField = document.querySelector("input[type='text']"); // Zugriff auf Input-Feld Element
     inputField.addEventListener("keyup", (event) => {
         if (event.key === "Enter") {
             let inputText = event.target.value; // Zugriff auf den eingegebenen Text im Input-Feld
-            event.target.value = "";
+            // (event.target as HTMLInputElement).value = "";
             console.log(`taskdefinition: ${inputText}`); // Konsolenausgabe des eingegebenen Textes
         }
     });
@@ -22,6 +22,24 @@ var organizer;
         let choosedDate = event.target.value;
         console.log(`you set the deadline: ${choosedDate}`);
     });
+    let commentField = document.getElementById('comment'); // Zugriff auf Input-Feld Element
+    commentField.addEventListener("keyup", (event) => {
+        if (event.key === "Enter") {
+            let inputComment = event.target.value; // Zugriff auf den eingegebenen Text im Input-Feld
+            // (event.target as HTMLInputElement).value = "";
+            console.log(`comment:${inputComment}`); // Konsolenausgabe des eingegebenen Textes
+        }
+    });
+    // hier id, da 2 input elemente vorhanden sind und nicht beide mit der gleichen klasse getrennt von einander funktionieren
+    let button = document.querySelector('.createtask');
+    button.addEventListener('click', function () {
+        let name = document.getElementById("select").value;
+        let date = document.querySelector("input[type='date']").value;
+        let inputField = document.getElementById("input").value;
+        let commentField = document.getElementById("comment").value;
+        console.log(name, inputField, date, commentField);
+    });
+    // löschen der zeile auskommentiert, da sonst buttonfunktion nicht funktioniert
 })(organizer || (organizer = {}));
 // Eingabe gewährleisten, Speicherung der Eingabe in Variablen oder consolen Ausgabe
 // Erstellen von p oder span bei enter also eventlistener auf felder mit keyboard und klick
--- L03_Aufgabenliste/script.ts
+++ L03_Aufgabenliste/script.ts
@@ -12,12 +12,12 @@ namespace organizer {
     // task.addEventListener('keyup', function(){
     //     console.log("you created a new task")
     // })
-        let inputField:HTMLInputElement = document.querySelector("input") as HTMLInputElement; // Zugriff auf Input-Feld Element
+        let inputField:HTMLInputElement = document.querySelector("input[type='text']") as HTMLInputElement; // Zugriff auf Input-Feld Element
       
         inputField.addEventListener("keyup", (event) => { // Hinzufügen des Eventlisteners für "keyup" - Ereignis
             if (event.key === "Enter"){
             let inputText:string = (event.target as HTMLInputElement).value; // Zugriff auf den eingegebenen Text im Input-Feld
-            (event.target as HTMLInputElement).value = "";
+            // (event.target as HTMLInputElement).value = "";
       
           console.log(`taskdefinition: ${inputText}`); // Konsolenausgabe des eingegebenen Textes
     }});
@@ -27,9 +27,30 @@ namespace organizer {
     date.addEventListener('change', (event) => {
         let choosedDate:string=(event.target as HTMLInputElement).value;
         console.log(`you set the deadline: ${choosedDate}`);
-    })    
+    })   
+    
+    let commentField:HTMLInputElement = document.getElementById('comment') as HTMLInputElement; // Zugriff auf Input-Feld Element
+      
+    commentField.addEventListener("keyup", (event) => { // Hinzufügen des Eventlisteners für "keyup" - Ereignis
+        if (event.key === "Enter"){
+        let inputComment:string = (event.target as HTMLInputElement).value; // Zugriff auf den eingegebenen Text im Input-Feld
+        // (event.target as HTMLInputElement).value = "";
+  
+      console.log(`comment:${inputComment}`); // Konsolenausgabe des eingegebenen Textes
+}});
+// hier id, da 2 input elemente vorhanden sind und nicht beide mit der gleichen klasse getrennt von einander funktionieren
 
+let button: HTMLButtonElement = document.querySelector('.createtask') as HTMLButtonElement;
 
+button.addEventListener('click', function(){
+  let name: string = (<HTMLSelectElement>document.getElementById("select")).value;
+  let date: string = (<HTMLInputElement>document.querySelector("input[type='date']")).value;
+  let inputField: string = (<HTMLSelectElement>document.getElementById("input")).value;
+  let commentField: string = (<HTMLSelectElement>document.getElementById("comment")).value;
+
+  console.log(name, inputField, date, commentField);
+});
+// löschen der zeile auskommentiert, da sonst buttonfunktion nicht funktioniert
 
 
 
