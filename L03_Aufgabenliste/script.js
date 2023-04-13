d23f59d622cc1c37218fd1c45a4f70457ce0efc4
Dominik-art-cpu <115712617+Dominik-hfu@users.noreply.github.com>
2023-04-13 18:10:45 +0200

    aufgabenliste

--- L03_Aufgabenliste/script.js
+++ L03_Aufgabenliste/script.js
@@ -8,6 +8,7 @@ Quellen: <->
 */
 var organizer;
 (function (organizer) {
+    let todos = document.querySelector(".todo");
     let name = document.querySelector('select');
     name.addEventListener('change', function () {
         let selectedName = name.options[name.selectedIndex].textContent;
@@ -41,15 +42,50 @@ var organizer;
     });
     // hier id, da 2 input elemente vorhanden sind und nicht beide mit der gleichen klasse getrennt von einander funktionieren
     let button = document.querySelector('.createtask');
+    let nameSelect = document.getElementById("select");
+    let dateInput = document.querySelector("input[type='date']");
     button.addEventListener('click', function () {
-        let nameSelect = document.getElementById("select");
-        let selectedName = nameSelect.options[nameSelect.selectedIndex].textContent;
-        let date = document.querySelector("input[type='date']").value;
-        console.log(selectedName, taskName, date, comment);
+        if (name.value != "", inputField.value != "", date.value != "", commentField.value != "") {
+            let newtask = document.createElement('span');
+            newtask.classList.add('task');
+            newtask.addEventListener('click', function () {
+                let window = document.createElement('span');
+                window.classList.add('window');
+                newtask.appendChild(window);
+            });
+            let selectedName = nameSelect.options[nameSelect.selectedIndex].textContent;
+            nameSelect.value = '';
+            let inputText = inputField.value;
+            inputField.value = "";
+            let date = document.querySelector("input[type='date']").value;
+            dateInput.value = "";
+            let comment = commentField.value;
+            commentField.value = "";
+            let trash = document.createElement('i');
+            trash.classList.add('fa-regular', 'fa-trash-can', 'fa-2x', 'trash', 'task');
+            trash.addEventListener('click', function () {
+                newtask.remove();
+                trash.remove();
+            });
+            newtask.textContent = selectedName + '' + inputText + '' + date + '' + comment;
+            todos.prepend(newtask);
+            newtask.append(trash);
+            console.log(selectedName, taskName, date, comment);
+        }
+        else {
+            alert('Bitte füllen Sie alle Felder aus');
+        }
     });
     let trash = document.getElementById('trash');
     trash.addEventListener('click', function () {
         console.clear();
+    });
+    let garbage = document.getElementById('trash');
+    garbage.addEventListener('click', function () {
+        nameSelect.value = '';
+        inputField.value = "";
+        dateInput.value = "";
+        commentField.value = "";
     });
     let garbage1 = document.querySelector('.garbage1');
     // // Keine Ahnung was für ein Typ :/
--- L03_Aufgabenliste/script.ts
+++ L03_Aufgabenliste/script.ts
@@ -8,86 +8,145 @@ Quellen: <->
 
 namespace organizer {
 
-    let name: HTMLSelectElement = document.querySelector('select') as HTMLSelectElement;
-    name.addEventListener('change', function () {
-      let selectedName:string|null= name.options[name.selectedIndex].textContent;
-        console.log("you choosed: " + selectedName)
+  let todos = document.querySelector(".todo") as HTMLDivElement;
+
+
+  let name: HTMLSelectElement = document.querySelector('select') as HTMLSelectElement;
+  name.addEventListener('change', function () {
+    let selectedName: string | null = name.options[name.selectedIndex].textContent;
+    console.log("you choosed: " + selectedName)
+  }
+  )
+  // possibly null :/
+
+  let taskName: string;
+  let comment: string;
+
+
+  let inputField: HTMLInputElement = document.querySelector("input[type='text']") as HTMLInputElement;
+
+  inputField.addEventListener("keyup", (event) => {
+    if (event.key === "Enter") {
+      let inputText: string = (event.target as HTMLInputElement).value;
+      taskName = inputText;
+
+      (event.target as HTMLInputElement).value = "";
+
+      console.log(`taskdefinition: ${inputText}`);
     }
-    )
-    // possibly null :/
-
-let taskName:string;
-let comment:string;
-
-        let inputField:HTMLInputElement = document.querySelector("input[type='text']") as HTMLInputElement;
-      
-        inputField.addEventListener("keyup", (event) => {
-            if (event.key === "Enter"){
-            let inputText:string = (event.target as HTMLInputElement).value;
-            taskName = inputText;
-
-            (event.target as HTMLInputElement).value = "";
-      
-          console.log(`taskdefinition: ${inputText}`);
-    }});
+  });
 
 
-    let date:HTMLInputElement= document.querySelector("input[type='date']") as HTMLInputElement;
-    date.addEventListener('change', (event) => {
-        let choosedDate:string=(event.target as HTMLInputElement).value;
-        console.log(`you set the deadline: ${choosedDate}`);
-    })   
-    
-    let commentField:HTMLInputElement = document.getElementById('comment') as HTMLInputElement; // Zugriff auf Input-Feld Element
-      
-    commentField.addEventListener("keyup", (event) => { // Hinzufügen des Eventlisteners für "keyup" - Ereignis
-        if (event.key === "Enter"){
-        let inputComment:string = (event.target as HTMLInputElement).value; // Zugriff auf den eingegebenen Text im Input-Feld
-        comment=inputComment;
-        (event.target as HTMLInputElement).value = "";
-  
+  let date: HTMLInputElement = document.querySelector("input[type='date']") as HTMLInputElement;
+  date.addEventListener('change', (event) => {
+    let choosedDate: string = (event.target as HTMLInputElement).value;
+    console.log(`you set the deadline: ${choosedDate}`);
+  })
+
+  let commentField: HTMLInputElement = document.getElementById('comment') as HTMLInputElement; // Zugriff auf Input-Feld Element
+
+  commentField.addEventListener("keyup", (event) => { // Hinzufügen des Eventlisteners für "keyup" - Ereignis
+    if (event.key === "Enter") {
+      let inputComment: string = (event.target as HTMLInputElement).value; // Zugriff auf den eingegebenen Text im Input-Feld
+      comment = inputComment;
+      (event.target as HTMLInputElement).value = "";
+
       console.log(`comment:${inputComment}`);
-}});
-// hier id, da 2 input elemente vorhanden sind und nicht beide mit der gleichen klasse getrennt von einander funktionieren
+    }
+  });
+  // hier id, da 2 input elemente vorhanden sind und nicht beide mit der gleichen klasse getrennt von einander funktionieren
 
-let button: HTMLButtonElement = document.querySelector('.createtask') as HTMLButtonElement;
+  let button: HTMLButtonElement = document.querySelector('.createtask') as HTMLButtonElement;
 
-button.addEventListener('click', function(){
   let nameSelect: HTMLSelectElement = document.getElementById("select") as HTMLSelectElement;
-  let selectedName: string|null = nameSelect.options[nameSelect.selectedIndex].textContent;
-  let date: string = (<HTMLInputElement>document.querySelector("input[type='date']")).value;
+  let dateInput: HTMLInputElement = document.querySelector("input[type='date']") as HTMLInputElement;
 
-  console.log(selectedName, taskName, date, comment);
-});
-let trash:HTMLLIElement= document.getElementById('trash') as HTMLLIElement;
-trash.addEventListener('click', function(){
-  console.clear()
-})
+  button.addEventListener('click', function () {
 
-let garbage1=document.querySelector('.garbage1');
-// // Keine Ahnung was für ein Typ :/
-garbage1.addEventListener('click', function(){
-      let deleteTask = document.querySelector('.task1');
-      deleteTask.remove();
-      console.log('you deleted task 1');
-})
-let garbage2=document.querySelector('.garbage2');
-garbage2.addEventListener('click', function(){
-      let deleteTask = document.querySelector('.task2');
-      deleteTask.remove();
-      console.log('you deleted task 2');
-    })
-    let garbage3=document.querySelector('.garbage3');
-    garbage3.addEventListener('click', function(){
-      let deleteTask = document.querySelector('.task3');
-      deleteTask.remove();
-      console.log('you deleted task 3');
-})
-// is possibly null :/
-// musste ich leider so machen, da ich nicht wusste wie ich sonst den Zugriff auf jeden einzelnen Mülleimer und Zeile hätte
+    if (name.value != "", inputField.value != "", date.value != "", commentField.value != "") {
 
-// Formatierung ging leider nicht schöner :/
-// Rahmen ist irgendwie kaputt
+      let newtask: HTMLSpanElement = document.createElement('span');
+      newtask.classList.add('task');
+      newtask.addEventListener('click',function(){
+        let window:HTMLSpanElement=document.createElement('span')as HTMLSpanElement;
+        window.classList.add('window');
+        newtask.appendChild(window)
+      })
+
+
+      let selectedName: string | null = nameSelect.options[nameSelect.selectedIndex].textContent;
+      nameSelect.value = '';
+      
+      let inputText = inputField.value;
+      inputField.value = "";
+
+      let date: string = (<HTMLInputElement>document.querySelector("input[type='date']")).value;
+      dateInput.value = "";
+      
+      
+      let comment: string = commentField.value
+      commentField.value = ""
+      
+      let trash: HTMLLIElement = document.createElement('i') as HTMLLIElement;
+      trash.classList.add('fa-regular', 'fa-trash-can', 'fa-2x', 'trash', 'task');
+      trash.addEventListener('click', function () {
+        newtask.remove()
+        trash.remove()
+      })
+      
+      newtask.textContent = selectedName + '' +inputText + '' + date + '' + comment;
+
+      todos.prepend(newtask);
+      newtask.append(trash);
+      
+      
+      console.log(selectedName, taskName, date, comment);
+    }
+    else{
+    alert('Bitte füllen Sie alle Felder aus')
+  }});
+
+  let trash: HTMLLIElement = document.getElementById('trash') as HTMLLIElement;
+  trash.addEventListener('click', function () {
+    console.clear()
+  })
+
+
+  let garbage:HTMLLIElement=document.getElementById('trash')as HTMLLIElement;
+  garbage.addEventListener('click', function(){
+
+    nameSelect.value = '';
+    inputField.value = "";
+    dateInput.value = "";
+    commentField.value = "";
+
+
+  })
+
+  let garbage1 = document.querySelector('.garbage1');
+  // // Keine Ahnung was für ein Typ :/
+  garbage1.addEventListener('click', function () {
+    let deleteTask = document.querySelector('.task1');
+    deleteTask.remove();
+    console.log('you deleted task 1');
+  })
+  let garbage2 = document.querySelector('.garbage2');
+  garbage2.addEventListener('click', function () {
+    let deleteTask = document.querySelector('.task2');
+    deleteTask.remove();
+    console.log('you deleted task 2');
+  })
+  let garbage3 = document.querySelector('.garbage3');
+  garbage3.addEventListener('click', function () {
+    let deleteTask = document.querySelector('.task3');
+    deleteTask.remove();
+    console.log('you deleted task 3');
+  })
+  // is possibly null :/
+  // musste ich leider so machen, da ich nicht wusste wie ich sonst den Zugriff auf jeden einzelnen Mülleimer und Zeile hätte
+
+  // Formatierung ging leider nicht schöner :/
+  // Rahmen ist irgendwie kaputt
 
 
 }
