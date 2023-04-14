9fabb69d2eb9cd800bb666f3ae9fcad478457665
Dominik-art-cpu <115712617+Dominik-hfu@users.noreply.github.com>
2023-04-14 19:21:40 +0200

    aufgabenliste

--- L03_Aufgabenliste/script.js
+++ L03_Aufgabenliste/script.js
@@ -1,20 +1,19 @@
 "use strict";
 /*
-Aufgabe: <L03_Aufgabenliste>
+Aufgabe: <L04_Aufgabenliste>
 Name: <Dominik Putz>
 Matrikel: <272244>
-Datum: <07.04.2023>
+Datum: <14.04.2023>
 Quellen: <->
 */
 var organizer;
 (function (organizer) {
     let todos = document.querySelector(".todo");
     let name = document.querySelector('select');
-    name.addEventListener('change', function () {
+    name.addEventListener('change', function selectname() {
         let selectedName = name.options[name.selectedIndex].textContent;
         console.log("you choosed: " + selectedName);
     });
-    // possibly null :/
     let taskName;
     let comment;
     let inputField = document.querySelector("input[type='text']");
@@ -39,79 +38,112 @@ var organizer;
             event.target.value = "";
             console.log(`comment:${inputComment}`);
         }
+        ;
     });
     // hier id, da 2 input elemente vorhanden sind und nicht beide mit der gleichen klasse getrennt von einander funktionieren
     let button = document.querySelector('.createtask');
     let nameSelect = document.getElementById("select");
     let dateInput = document.querySelector("input[type='date']");
-    button.addEventListener('click', function () {
-        if (name.value != "", inputField.value != "", date.value != "", commentField.value != "") {
-            let newtask = document.createElement('span');
+    button.addEventListener('click', function createTask() {
+        if (name.value !== "" && inputField.value != "" && date.value !== "" && commentField.value != "") {
+            // Variablen zum Speichern der ausgewählten Werte
+            let selectedName = nameSelect.options[nameSelect.selectedIndex].textContent;
+            let inputText = inputField.value;
+            let inputDate = dateInput.value;
+            let newtask = document.createElement('fieldset');
             newtask.classList.add('task');
             newtask.addEventListener('click', function () {
-                let window = document.createElement('span');
-                window.classList.add('window');
-                newtask.appendChild(window);
+                let i = true;
+                // let j:number=0;
+                // for(j=0; j++, j<10;)
+                // es muss immer wieder überprüft werden ob der span schon geklickt wurde/ erster klicken anzeigen, zweiter klick ausblenden
+                if (i == true) {
+                    let window = document.createElement('span');
+                    window.classList.add('window');
+                    i = false;
+                    newtask.appendChild(window);
+                    window.appendChild(nameSpan);
+                    // Name rutscht weg
+                    // If Bedingung lässt zu das nur beim ersten geklickt werden kann
+                }
             });
-            let selectedName = nameSelect.options[nameSelect.selectedIndex].textContent;
+            let nameSpan = document.createElement('span');
+            nameSpan.textContent = selectedName;
+            newtask.appendChild(nameSpan);
             nameSelect.value = '';
-            let inputText = inputField.value;
+            let taskSpan = document.createElement('span');
+            taskSpan.textContent = inputText;
+            newtask.appendChild(taskSpan);
             inputField.value = "";
-            let date = document.querySelector("input[type='date']").value;
+            let dateSpan = document.createElement('span');
+            dateSpan.textContent = inputDate;
+            newtask.appendChild(dateSpan);
             dateInput.value = "";
-            let comment = commentField.value;
+            const selectedDate = new Date(dateInput.value);
+            const currentDate = new Date();
+            const isDateInThePast = selectedDate < currentDate;
+            if (selectedDate.valueOf() && dateInput.value && !isDateInThePast) {
+                const fieldset = document.createElement('fieldset');
+                fieldset.classList.add('timeIsUp');
+            }
+            // versuch bei abgelaufenem Datum das Fieldset rot zu färben
+            let commentSpan = document.createElement('span');
+            commentSpan.classList.add('comment');
+            commentSpan.textContent = commentField.value;
+            newtask.appendChild(commentSpan);
             commentField.value = "";
             let trash = document.createElement('i');
             trash.classList.add('fa-regular', 'fa-trash-can', 'fa-2x', 'trash', 'task');
-            trash.addEventListener('click', function () {
+            trash.addEventListener('click', function Delete() {
                 newtask.remove();
                 trash.remove();
             });
-            newtask.textContent = selectedName + '' + inputText + '' + date + '' + comment;
+            newtask.appendChild(trash);
             todos.prepend(newtask);
-            newtask.append(trash);
             console.log(selectedName, taskName, date, comment);
         }
         else {
             alert('Bitte füllen Sie alle Felder aus');
         }
     });
-    let trash = document.getElementById('trash');
-    trash.addEventListener('click', function () {
-        console.clear();
-    });
+    // let trash: HTMLLIElement = document.getElementById('trash') as HTMLLIElement;
+    // trash.addEventListener('click', function () {
+    //   console.clear()
+    // })
     let garbage = document.getElementById('trash');
-    garbage.addEventListener('click', function () {
-        nameSelect.value = '';
+    garbage.addEventListener('click', function DeleteInput() {
+        nameSelect.value = "";
         inputField.value = "";
         dateInput.value = "";
         commentField.value = "";
     });
     let garbage1 = document.querySelector('.garbage1');
-    // // Keine Ahnung was für ein Typ :/
     garbage1.addEventListener('click', function () {
         let deleteTask = document.querySelector('.task1');
-        deleteTask.remove();
+        deleteTask?.remove();
         console.log('you deleted task 1');
     });
     let garbage2 = document.querySelector('.garbage2');
     garbage2.addEventListener('click', function () {
         let deleteTask = document.querySelector('.task2');
-        deleteTask.remove();
+        deleteTask?.remove();
         console.log('you deleted task 2');
     });
     let garbage3 = document.querySelector('.garbage3');
     garbage3.addEventListener('click', function () {
         let deleteTask = document.querySelector('.task3');
-        deleteTask.remove();
+        deleteTask?.remove();
         console.log('you deleted task 3');
     });
-    // is possibly null :/
-    // musste ich leider so machen, da ich nicht wusste wie ich sonst den Zugriff auf jeden einzelnen Mülleimer und Zeile hätte
-    // Formatierung ging leider nicht schöner :/
-    // Rahmen ist irgendwie kaputt
+    // Frage- und Ausrufezeichen um Fehler possibly null zu beheben
 })(organizer || (organizer = {}));
-//Speicherung der Eingabe in Variablen
-// Erstellen von p oder span bei Enter und Erstellen Button
-// child appenden
+;
+// Rahmen ist irgendwie kaputt :/
+// Datum umdrehen?
+// Dropdown mit range input element (status)
+// Dropdown mit einem klick auf beim zweiten klick zu (ungerade und gerade wie bei haken todoapp)
+// Zeit abgelaufen task wird rot markiert
+// Noch 3 Tage Zeit Task grün
+// Noch 1 Tag Zeit Task Orange
+// Aufgabe kann nur gelöscht werden, wenn diese erledigt ist oder es kommt alert wenn nicht erledigte aufgabe gelöscht wird oder nachfrage diese wirklich löschen?
 //# sourceMappingURL=script.js.map
--- L03_Aufgabenliste/script.ts
+++ L03_Aufgabenliste/script.ts
@@ -1,8 +1,8 @@
 /*
-Aufgabe: <L03_Aufgabenliste>
+Aufgabe: <L04_Aufgabenliste>
 Name: <Dominik Putz>
 Matrikel: <272244>
-Datum: <07.04.2023>
+Datum: <14.04.2023>
 Quellen: <->
 */
 
@@ -12,16 +12,13 @@ namespace organizer {
 
 
   let name: HTMLSelectElement = document.querySelector('select') as HTMLSelectElement;
-  name.addEventListener('change', function () {
+  name.addEventListener('change', function selectname() {
     let selectedName: string | null = name.options[name.selectedIndex].textContent;
-    console.log("you choosed: " + selectedName)
-  }
-  )
-  // possibly null :/
+    console.log("you choosed: " + selectedName);
+  });
 
   let taskName: string;
   let comment: string;
-
 
   let inputField: HTMLInputElement = document.querySelector("input[type='text']") as HTMLInputElement;
 
@@ -36,12 +33,11 @@ namespace organizer {
     }
   });
 
-
   let date: HTMLInputElement = document.querySelector("input[type='date']") as HTMLInputElement;
   date.addEventListener('change', (event) => {
     let choosedDate: string = (event.target as HTMLInputElement).value;
     console.log(`you set the deadline: ${choosedDate}`);
-  })
+  });
 
   let commentField: HTMLInputElement = document.getElementById('comment') as HTMLInputElement; // Zugriff auf Input-Feld Element
 
@@ -52,7 +48,7 @@ namespace organizer {
       (event.target as HTMLInputElement).value = "";
 
       console.log(`comment:${inputComment}`);
-    }
+    };
   });
   // hier id, da 2 input elemente vorhanden sind und nicht beide mit der gleichen klasse getrennt von einander funktionieren
 
@@ -60,96 +56,136 @@ namespace organizer {
 
   let nameSelect: HTMLSelectElement = document.getElementById("select") as HTMLSelectElement;
   let dateInput: HTMLInputElement = document.querySelector("input[type='date']") as HTMLInputElement;
-
-  button.addEventListener('click', function () {
-
-    if (name.value != "", inputField.value != "", date.value != "", commentField.value != "") {
-
-      let newtask: HTMLSpanElement = document.createElement('span');
-      newtask.classList.add('task');
-      newtask.addEventListener('click',function(){
-        let window:HTMLSpanElement=document.createElement('span')as HTMLSpanElement;
-        window.classList.add('window');
-        newtask.appendChild(window)
-      })
-
-
-      let selectedName: string | null = nameSelect.options[nameSelect.selectedIndex].textContent;
-      nameSelect.value = '';
+  
+  button.addEventListener('click', function createTask() {
+    
+    if (name.value !== ""&& inputField.value != ""&& date.value !== ""&& commentField.value != "") {
       
-      let inputText = inputField.value;
+      // Variablen zum Speichern der ausgewählten Werte
+      let selectedName: string | null = nameSelect.options[nameSelect.selectedIndex].textContent;
+      let inputText: string = inputField.value;
+      let inputDate: string = dateInput.value;
+      
+      let newtask: HTMLFieldSetElement = document.createElement('fieldset');
+      newtask.classList.add('task');
+      newtask.addEventListener('click', function () {
+        let i:boolean=true;
+        // let j:number=0;
+        // for(j=0; j++, j<10;)
+        // es muss immer wieder überprüft werden ob der span schon geklickt wurde/ erster klicken anzeigen, zweiter klick ausblenden
+        if(i==true){
+        let window: HTMLSpanElement = document.createElement('span') as HTMLSpanElement;
+        window.classList.add('window');
+          i=false;
+        newtask.appendChild(window);
+        window.appendChild(nameSpan);
+        // Name rutscht weg
+        // If Bedingung lässt zu das nur beim ersten geklickt werden kann
+      }});
+
+      let nameSpan: HTMLSpanElement = document.createElement('span');
+      nameSpan.textContent = selectedName;
+      newtask.appendChild(nameSpan);
+      nameSelect.value = '';
+
+      let taskSpan: HTMLSpanElement = document.createElement('span');
+      taskSpan.textContent = inputText;
+      newtask.appendChild(taskSpan);
       inputField.value = "";
 
-      let date: string = (<HTMLInputElement>document.querySelector("input[type='date']")).value;
+      let dateSpan: HTMLSpanElement = document.createElement('span');
+      dateSpan.textContent = inputDate;
+      newtask.appendChild(dateSpan);
       dateInput.value = "";
-      
-      
-      let comment: string = commentField.value
-      commentField.value = ""
-      
+
+
+const selectedDate = new Date(dateInput.value);
+const currentDate = new Date();
+const isDateInThePast = selectedDate < currentDate;
+
+if (selectedDate.valueOf() && dateInput.value && !isDateInThePast) {
+  const fieldset = document.createElement('fieldset');
+  fieldset.classList.add('timeIsUp');
+}
+// versuch bei abgelaufenem Datum das Fieldset rot zu färben
+
+
+
+      let commentSpan: HTMLSpanElement = document.createElement('span');
+      commentSpan.classList.add('comment');
+      commentSpan.textContent = commentField.value;
+      newtask.appendChild(commentSpan);
+      commentField.value = "";
+
+
       let trash: HTMLLIElement = document.createElement('i') as HTMLLIElement;
       trash.classList.add('fa-regular', 'fa-trash-can', 'fa-2x', 'trash', 'task');
-      trash.addEventListener('click', function () {
-        newtask.remove()
-        trash.remove()
-      })
-      
-      newtask.textContent = selectedName + '' +inputText + '' + date + '' + comment;
+      trash.addEventListener('click', function Delete() {
+        newtask.remove();
+        trash.remove();
 
+      });
+
+      newtask.appendChild(trash);
       todos.prepend(newtask);
-      newtask.append(trash);
-      
-      
+
       console.log(selectedName, taskName, date, comment);
     }
-    else{
-    alert('Bitte füllen Sie alle Felder aus')
-  }});
 
-  let trash: HTMLLIElement = document.getElementById('trash') as HTMLLIElement;
-  trash.addEventListener('click', function () {
-    console.clear()
-  })
+    else {
 
+      alert('Bitte füllen Sie alle Felder aus');
 
-  let garbage:HTMLLIElement=document.getElementById('trash')as HTMLLIElement;
-  garbage.addEventListener('click', function(){
+    }
+  });
 
-    nameSelect.value = '';
+  // let trash: HTMLLIElement = document.getElementById('trash') as HTMLLIElement;
+  // trash.addEventListener('click', function () {
+  //   console.clear()
+  // })
+
+  let garbage: HTMLLIElement = document.getElementById('trash') as HTMLLIElement;
+  garbage.addEventListener('click', function DeleteInput() {
+
+    nameSelect.value = "";
     inputField.value = "";
     dateInput.value = "";
     commentField.value = "";
 
 
-  })
+  });
 
-  let garbage1 = document.querySelector('.garbage1');
-  // // Keine Ahnung was für ein Typ :/
+  let garbage1: HTMLLIElement = document.querySelector('.garbage1')!;
   garbage1.addEventListener('click', function () {
     let deleteTask = document.querySelector('.task1');
-    deleteTask.remove();
+    deleteTask?.remove();
     console.log('you deleted task 1');
-  })
-  let garbage2 = document.querySelector('.garbage2');
+  });
+
+  let garbage2: HTMLLIElement = document.querySelector('.garbage2')!;
   garbage2.addEventListener('click', function () {
     let deleteTask = document.querySelector('.task2');
-    deleteTask.remove();
+    deleteTask?.remove();
     console.log('you deleted task 2');
-  })
-  let garbage3 = document.querySelector('.garbage3');
+
+  });
+
+  let garbage3: HTMLLIElement = document.querySelector('.garbage3')!;
   garbage3.addEventListener('click', function () {
     let deleteTask = document.querySelector('.task3');
-    deleteTask.remove();
+    deleteTask?.remove();
     console.log('you deleted task 3');
-  })
-  // is possibly null :/
-  // musste ich leider so machen, da ich nicht wusste wie ich sonst den Zugriff auf jeden einzelnen Mülleimer und Zeile hätte
 
-  // Formatierung ging leider nicht schöner :/
-  // Rahmen ist irgendwie kaputt
+  });
+  // Frage- und Ausrufezeichen um Fehler possibly null zu beheben
 
+};
+// Rahmen ist irgendwie kaputt :/
+// Datum umdrehen?
 
-}
-//Speicherung der Eingabe in Variablen
-// Erstellen von p oder span bei Enter und Erstellen Button
-// child appenden
+// Dropdown mit range input element (status)
+// Dropdown mit einem klick auf beim zweiten klick zu (ungerade und gerade wie bei haken todoapp)
+// Zeit abgelaufen task wird rot markiert
+// Noch 3 Tage Zeit Task grün
+// Noch 1 Tag Zeit Task Orange
+// Aufgabe kann nur gelöscht werden, wenn diese erledigt ist oder es kommt alert wenn nicht erledigte aufgabe gelöscht wird oder nachfrage diese wirklich löschen?
