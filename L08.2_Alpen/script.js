f368ad9df952f58759d21cee3ebed22adcf08f88
Dominik-art-cpu <115712617+Dominik-hfu@users.noreply.github.com>
2023-05-09 18:38:39 +0200

    begin_08.1

---
+++ L08.2_Alpen/script.js
@@ -0,0 +1,26 @@
+"use strict";
+var Alpen;
+(function (Alpen) {
+    window.addEventListener("load", handleload);
+    let crc2;
+    let canvas = document.querySelector("canvas");
+    function handleload(_event) {
+        crc2 = canvas.getContext("2d");
+        background();
+    }
+    function background() {
+        let gradient = crc2.createLinearGradient(100, 0, 0, 0);
+        gradient.addColorStop(0, "#51d9ed");
+        // gradient.addColorStop(.5, "hsl (220, 80%, 80%)");
+        gradient.addColorStop(1, "#000000");
+        crc2.beginPath();
+        crc2.fillStyle = gradient;
+        crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
+        crc2.closePath();
+    }
+    //mit translate verschiebt man das allgemeine koordinatensystem vom ursprung 0,0 zu einer bestimmten position
+    // Koordinatensystem könnte auch mit *(-1) umgekehrt werden
+    // In der Funktion muss die Translation am Ende wieder Rückgängig gemacht werden mit safe und restore ansonsten addieren sich die Koordinatensysteme auf
+    // Oder mit reset transform
+})(Alpen || (Alpen = {}));
+//# sourceMappingURL=script.js.map
