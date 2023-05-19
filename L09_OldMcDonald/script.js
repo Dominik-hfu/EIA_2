c0f2bc45fd01dd8a1037bd641ae49a7c86bad73d
Dominik-art-cpu <115712617+Dominik-hfu@users.noreply.github.com>
2023-05-19 11:08:06 +0200

    oldmcdonald

---
+++ L09_OldMcDonald/script.js
@@ -0,0 +1,98 @@
+"use strict";
+var OldMcDonald;
+(function (OldMcDonald) {
+    window.addEventListener("load", handleload);
+    let crc2;
+    let canvas = document.querySelector("canvas");
+    function handleload(_event) {
+        crc2 = canvas.getContext("2d");
+        background();
+        sun();
+        cloud({ x: 100, y: 100 });
+        // drawField();
+    }
+    ;
+    // function drawField(){
+    //     let pattern: CanvasRenderingContext2D =<CanvasRenderingContext2D> document.createElement('canvas').getContext('2d');
+    // pattern.canvas.width = 40;
+    // pattern.canvas.height = 20;
+    // pattern.fillStyle = '#fec';
+    // pattern.fillRect(0, 0, pattern.canvas.width, pattern.canvas.height);
+    // pattern.moveTo(0, 10);
+    // pattern.lineTo(10, 10);
+    // pattern.lineTo(20, 0);
+    // pattern.lineTo(30, 0);
+    // pattern.lineTo(40, 10);
+    // pattern.lineTo(30, 20);
+    // pattern.lineTo(20, 20);
+    // pattern.lineTo(10, 10);
+    // pattern.stroke();
+    // crc2.fillStyle = <CanvasRenderingContext2D>crc2.createPattern(pattern.canvas, 'repeat');
+    // crc2.fillRect(0, 0, canvas.width, canvas.height);
+    // };
+    function cloud(position) {
+        let minSize = { x: 100, y: 100 };
+        let maxSize = { x: 250, y: 250 };
+        let size = {
+            x: minSize.x + Math.random() * (maxSize.x - minSize.x),
+            y: minSize.y + Math.random() * (maxSize.y - minSize.y)
+        };
+        let circles = 20;
+        let circleRadius = 50;
+        let particle = new Path2D();
+        let cloudGradient = crc2.createRadialGradient(0, 0, 0, 0, 0, circleRadius);
+        particle.arc(0, 0, circleRadius, 0, 2 * Math.PI);
+        cloudGradient.addColorStop(0, "HSLA(0, 100%, 100%, 0.5)");
+        cloudGradient.addColorStop(1, "HSLA(0, 100%, 100%, 0)");
+        crc2.save();
+        crc2.translate(position.x, position.y);
+        for (let circle = 0; circle < circles; circle++) {
+            crc2.save();
+            let x = Math.random() * size.x - size.x / 2;
+            let y = Math.random() * size.y - size.y / 2;
+            crc2.translate(x, y);
+            crc2.fillStyle = cloudGradient;
+            crc2.fill(particle);
+            crc2.restore();
+        }
+        crc2.restore();
+    }
+    function sun() {
+        let minSunRadius = 20;
+        let maxSunRadius = 50;
+        let sunHeight = Math.floor(Math.random() * (maxSunRadius - minSunRadius + 1)) + maxSunRadius;
+        let sunX = 900;
+        let sunY = 50;
+        let gradient = crc2.createRadialGradient(sunX, sunY, sunHeight + 60, sunX, sunY, sunHeight);
+        gradient.addColorStop(0, "HSLA(60, 100%, 50%, 0)");
+        gradient.addColorStop(1, "HSLA(60, 100%, 90%, 1)");
+        crc2.moveTo(sunX, sunY);
+        crc2.beginPath();
+        crc2.arc(sunX, sunY, sunHeight + 60, 0, 2 * Math.PI);
+        crc2.strokeStyle = "transparent";
+        crc2.fillStyle = gradient;
+        crc2.fill();
+        crc2.closePath();
+        crc2.moveTo(sunX, sunY);
+        crc2.beginPath();
+        crc2.arc(sunX, sunY, sunHeight, 0, 2 * Math.PI);
+        crc2.strokeStyle = "transparent";
+        crc2.fillStyle = gradient;
+        crc2.stroke();
+        crc2.closePath();
+    }
+    ;
+    function background() {
+        let gradient = crc2.createLinearGradient(0, 0, 0, crc2.canvas.height);
+        gradient.addColorStop(0, "#51d9ed");
+        gradient.addColorStop(.25, "HSL(220, 80%, 80%)");
+        gradient.addColorStop(.5, "HSL(129,60%,37%)");
+        gradient.addColorStop(1, "HSL(30, 100%, 30%)");
+        crc2.beginPath();
+        crc2.fillStyle = gradient;
+        crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
+        crc2.closePath();
+    }
+    ;
+})(OldMcDonald || (OldMcDonald = {}));
+//# sourceMappingURL=script.js.map
